ee99f90562a6b96bbae3c0a0f358d79ac215c661
sca1d <lol.scald.lol@gmail.com>
2022-06-09 14:43:02 +0900

    add description

--- libmop/include/effect.hpp
+++ libmop/include/effect.hpp
@@ -3,9 +3,28 @@
 
 namespace mop {
 
+	/**
+	* @fn
+	* @brief 画像データにのノイズを付加します。
+	* @param src ノイズを付加する画像データ
+	* @param percent 付加度
+	* @param seed ランダムシード値
+	*/
 	DLL_EXPORT void Noise(matrix* src, int percent, int seed = 0);
 
+	/**
+	* @fn
+	* @brief rgbデータをhsvデータに変換します。
+	* @param c rgbデータ
+	* @return hsvデータを返します。
+	*/
 	DLL_EXPORT hsv rgb2hsv(rgb c);
+	/**
+	* @fn
+	* @brief hsvデータをrgbデータに変換します。
+	* @param c hsvデータ
+	* @return rgbデータを返します。
+	*/
 	DLL_EXPORT rgb hsv2rgb(hsv c);
 
 }
